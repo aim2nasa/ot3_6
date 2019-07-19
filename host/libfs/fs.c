431083c21ac4edd9d1db3644999301778889de98
Rossi Kwak <rossi.kwak@gmail.com>
2019-07-19 18:22:39 +0900

    refactoring to use libsess

--- host/libfs/fs.c
+++ host/libfs/fs.c
@@ -6,21 +6,12 @@
 
 TEEC_UUID uuid = TA_STORAGE_UUID; 
 
-TEEC_Result initializeContext(const char *name,fs *f)
+TEEC_UUID * getUUID()
 {
-	f->ctx = (TEEC_Context*)malloc(sizeof(TEEC_Context));
-	return TEEC_InitializeContext(name,f->ctx);
+	return &uuid;
 }
 
-TEEC_Result openSession(fs *f,uint32_t connectionMethod,
-			const void *connectionData,TEEC_Operation *operation)
-{
-	f->session = (TEEC_Session*)malloc(sizeof(TEEC_Session));
-	return TEEC_OpenSession(f->ctx,f->session,&uuid,connectionMethod,
-		connectionData,operation,&f->error);
-}
-
-TEEC_Result fs_open(fs *f, void *id, uint32_t id_size,
+TEEC_Result fs_open(oc *o, void *id, uint32_t id_size,
 			   uint32_t flags, uint32_t *obj, uint32_t storage_id)
 {
 	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
@@ -37,7 +28,7 @@ TEEC_Result fs_open(fs *f, void *id, uint32_t id_size,
 					 TEEC_VALUE_INOUT, TEEC_VALUE_INPUT,
 					 TEEC_NONE);
 
-	res = TEEC_InvokeCommand(f->session, TA_STORAGE_CMD_OPEN, &op, &org);
+	res = TEEC_InvokeCommand(o->session, TA_STORAGE_CMD_OPEN, &op, &org);
 
 	if (res == TEEC_SUCCESS)
 		*obj = op.params[1].value.b;
@@ -45,7 +36,7 @@ TEEC_Result fs_open(fs *f, void *id, uint32_t id_size,
 	return res;
 }
 
-TEEC_Result fs_create(fs *f, void *id, uint32_t id_size,
+TEEC_Result fs_create(oc *o, void *id, uint32_t id_size,
 			     uint32_t flags, uint32_t attr, void *data,
 			     uint32_t data_size, uint32_t *obj,
 			     uint32_t storage_id)
@@ -67,7 +58,7 @@ TEEC_Result fs_create(fs *f, void *id, uint32_t id_size,
 					 TEEC_VALUE_INOUT, TEEC_VALUE_INPUT,
 					 TEEC_MEMREF_TEMP_INPUT);
 
-	res = TEEC_InvokeCommand(f->session, TA_STORAGE_CMD_CREATE, &op, &org);
+	res = TEEC_InvokeCommand(o->session, TA_STORAGE_CMD_CREATE, &op, &org);
 
 	if (res == TEEC_SUCCESS)
 		*obj = op.params[1].value.b;
@@ -75,7 +66,7 @@ TEEC_Result fs_create(fs *f, void *id, uint32_t id_size,
 	return res;
 }
 
-TEEC_Result fs_create_overwrite(fs *f, void *id,
+TEEC_Result fs_create_overwrite(oc *o, void *id,
 				       uint32_t id_size, uint32_t storage_id)
 {
 	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
@@ -90,12 +81,12 @@ TEEC_Result fs_create_overwrite(fs *f, void *id,
 					 TEEC_VALUE_INPUT, TEEC_NONE,
 					 TEEC_NONE);
 
-	res = TEEC_InvokeCommand(f->session, TA_STORAGE_CMD_CREATE_OVERWRITE, &op, &org);
+	res = TEEC_InvokeCommand(o->session, TA_STORAGE_CMD_CREATE_OVERWRITE, &op, &org);
 
 	return res;
 }
 
-TEEC_Result fs_close(fs *f, uint32_t obj)
+TEEC_Result fs_close(oc *o, uint32_t obj)
 {
 	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
 	uint32_t org = 0;
@@ -105,10 +96,10 @@ TEEC_Result fs_close(fs *f, uint32_t obj)
 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_NONE,
 					 TEEC_NONE, TEEC_NONE);
 
-	return TEEC_InvokeCommand(f->session, TA_STORAGE_CMD_CLOSE, &op, &org);
+	return TEEC_InvokeCommand(o->session, TA_STORAGE_CMD_CLOSE, &op, &org);
 }
 
-TEEC_Result fs_read(fs *f, uint32_t obj, void *data,
+TEEC_Result fs_read(oc *o, uint32_t obj, void *data,
 			   uint32_t data_size, uint32_t *count)
 {
 	TEEC_Result res = TEEC_ERROR_GENERIC;
@@ -124,7 +115,7 @@ TEEC_Result fs_read(fs *f, uint32_t obj, void *data,
 					 TEEC_VALUE_INOUT, TEEC_NONE,
 					 TEEC_NONE);
 
-	res = TEEC_InvokeCommand(f->session, TA_STORAGE_CMD_READ, &op, &org);
+	res = TEEC_InvokeCommand(o->session, TA_STORAGE_CMD_READ, &op, &org);
 
 	if (res == TEEC_SUCCESS)
 		*count = op.params[1].value.b;
@@ -132,7 +123,7 @@ TEEC_Result fs_read(fs *f, uint32_t obj, void *data,
 	return res;
 }
 
-TEEC_Result fs_write(fs *f, uint32_t obj, void *data,
+TEEC_Result fs_write(oc *o, uint32_t obj, void *data,
 			    uint32_t data_size)
 {
 	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
@@ -147,10 +138,10 @@ TEEC_Result fs_write(fs *f, uint32_t obj, void *data,
 					 TEEC_VALUE_INPUT, TEEC_NONE,
 					 TEEC_NONE);
 
-	return TEEC_InvokeCommand(f->session, TA_STORAGE_CMD_WRITE, &op, &org);
+	return TEEC_InvokeCommand(o->session, TA_STORAGE_CMD_WRITE, &op, &org);
 }
 
-TEEC_Result fs_seek(fs *f, uint32_t obj, int32_t offset,
+TEEC_Result fs_seek(oc *o, uint32_t obj, int32_t offset,
 			   int32_t whence)
 {
 	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
@@ -163,10 +154,10 @@ TEEC_Result fs_seek(fs *f, uint32_t obj, int32_t offset,
 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_VALUE_INOUT,
 					 TEEC_NONE, TEEC_NONE);
 
-	return TEEC_InvokeCommand(f->session, TA_STORAGE_CMD_SEEK, &op, &org);
+	return TEEC_InvokeCommand(o->session, TA_STORAGE_CMD_SEEK, &op, &org);
 }
 
-TEEC_Result fs_unlink(fs *f, uint32_t obj)
+TEEC_Result fs_unlink(oc *o, uint32_t obj)
 {
 	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
 	uint32_t org = 0;
@@ -176,10 +167,10 @@ TEEC_Result fs_unlink(fs *f, uint32_t obj)
 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_NONE,
 					 TEEC_NONE, TEEC_NONE);
 
-	return TEEC_InvokeCommand(f->session, TA_STORAGE_CMD_UNLINK, &op, &org);
+	return TEEC_InvokeCommand(o->session, TA_STORAGE_CMD_UNLINK, &op, &org);
 }
 
-TEEC_Result fs_trunc(fs *f, uint32_t obj, uint32_t len)
+TEEC_Result fs_trunc(oc *o, uint32_t obj, uint32_t len)
 {
 	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
 	uint32_t org = 0;
@@ -190,10 +181,10 @@ TEEC_Result fs_trunc(fs *f, uint32_t obj, uint32_t len)
 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_NONE,
 					 TEEC_NONE, TEEC_NONE);
 
-	return TEEC_InvokeCommand(f->session, TA_STORAGE_CMD_TRUNC, &op, &org);
+	return TEEC_InvokeCommand(o->session, TA_STORAGE_CMD_TRUNC, &op, &org);
 }
 
-TEEC_Result fs_rename(fs *f, uint32_t obj, void *id,
+TEEC_Result fs_rename(oc *o, uint32_t obj, void *id,
 			     uint32_t id_size)
 {
 	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
@@ -207,10 +198,10 @@ TEEC_Result fs_rename(fs *f, uint32_t obj, void *id,
 					 TEEC_MEMREF_TEMP_INPUT, TEEC_NONE,
 					 TEEC_NONE);
 
-	return TEEC_InvokeCommand(f->session, TA_STORAGE_CMD_RENAME, &op, &org);
+	return TEEC_InvokeCommand(o->session, TA_STORAGE_CMD_RENAME, &op, &org);
 }
 
-TEEC_Result fs_alloc_enum(fs *f, uint32_t *e)
+TEEC_Result fs_alloc_enum(oc *o, uint32_t *e)
 {
 	TEEC_Result res = TEEC_ERROR_GENERIC;
 	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
@@ -219,7 +210,7 @@ TEEC_Result fs_alloc_enum(fs *f, uint32_t *e)
 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_OUTPUT, TEEC_NONE,
 					 TEEC_NONE, TEEC_NONE);
 
-	res = TEEC_InvokeCommand(f->session, TA_STORAGE_CMD_ALLOC_ENUM, &op, &org);
+	res = TEEC_InvokeCommand(o->session, TA_STORAGE_CMD_ALLOC_ENUM, &op, &org);
 
 	if (res == TEEC_SUCCESS)
 		*e = op.params[0].value.a;
@@ -227,7 +218,7 @@ TEEC_Result fs_alloc_enum(fs *f, uint32_t *e)
 	return res;
 }
 
-TEEC_Result fs_reset_enum(fs *f, uint32_t e)
+TEEC_Result fs_reset_enum(oc *o, uint32_t e)
 {
 	TEEC_Result res = TEEC_ERROR_GENERIC;
 	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
@@ -237,12 +228,12 @@ TEEC_Result fs_reset_enum(fs *f, uint32_t e)
 					 TEEC_NONE, TEEC_NONE);
 
 	op.params[0].value.a = e;
-	res = TEEC_InvokeCommand(f->session, TA_STORAGE_CMD_RESET_ENUM, &op, &org);
+	res = TEEC_InvokeCommand(o->session, TA_STORAGE_CMD_RESET_ENUM, &op, &org);
 
 	return res;
 }
 
-TEEC_Result fs_free_enum(fs *f, uint32_t e)
+TEEC_Result fs_free_enum(oc *o, uint32_t e)
 {
 	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
 	uint32_t org = 0;
@@ -252,10 +243,10 @@ TEEC_Result fs_free_enum(fs *f, uint32_t e)
 
 	op.params[0].value.a = e;
 
-	return TEEC_InvokeCommand(f->session, TA_STORAGE_CMD_FREE_ENUM, &op, &org);
+	return TEEC_InvokeCommand(o->session, TA_STORAGE_CMD_FREE_ENUM, &op, &org);
 }
 
-TEEC_Result fs_start_enum(fs *f, uint32_t e,
+TEEC_Result fs_start_enum(oc *o, uint32_t e,
 				 uint32_t storage_id)
 {
 	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
@@ -267,10 +258,10 @@ TEEC_Result fs_start_enum(fs *f, uint32_t e,
 	op.params[0].value.a = e;
 	op.params[0].value.b = storage_id;
 
-	return TEEC_InvokeCommand(f->session, TA_STORAGE_CMD_START_ENUM, &op, &org);
+	return TEEC_InvokeCommand(o->session, TA_STORAGE_CMD_START_ENUM, &op, &org);
 }
 
-TEEC_Result fs_next_enum(fs *f, uint32_t e, void *obj_info,
+TEEC_Result fs_next_enum(oc *o, uint32_t e, void *obj_info,
 				size_t info_size, void *id, uint32_t id_size)
 {
 	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
@@ -287,10 +278,10 @@ TEEC_Result fs_next_enum(fs *f, uint32_t e, void *obj_info,
 	op.params[2].tmpref.buffer = id;
 	op.params[2].tmpref.size = id_size;
 
-	return TEEC_InvokeCommand(f->session, TA_STORAGE_CMD_NEXT_ENUM, &op, &org);
+	return TEEC_InvokeCommand(o->session, TA_STORAGE_CMD_NEXT_ENUM, &op, &org);
 }
 
-TEEC_Result fs_restrict_usage(fs *f, uint32_t obj,
+TEEC_Result fs_restrict_usage(oc *o, uint32_t obj,
 				     uint32_t obj_usage)
 {
 	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
@@ -302,11 +293,11 @@ TEEC_Result fs_restrict_usage(fs *f, uint32_t obj,
 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_NONE,
 					 TEEC_NONE, TEEC_NONE);
 
-	return TEEC_InvokeCommand(f->session, TA_STORAGE_CMD_RESTRICT_USAGE,
+	return TEEC_InvokeCommand(o->session, TA_STORAGE_CMD_RESTRICT_USAGE,
 				  &op, &org);
 }
 
-TEEC_Result fs_alloc_obj(fs *f, uint32_t obj_type,
+TEEC_Result fs_alloc_obj(oc *o, uint32_t obj_type,
 				     uint32_t max_key_size, uint32_t *obj)
 {
 	TEEC_Result res = TEEC_ERROR_GENERIC;
@@ -319,12 +310,12 @@ TEEC_Result fs_alloc_obj(fs *f, uint32_t obj_type,
 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_VALUE_OUTPUT,
 					 TEEC_NONE, TEEC_NONE);
 
-	res = TEEC_InvokeCommand(f->session, TA_STORAGE_CMD_ALLOC_OBJ, &op, &org);
+	res = TEEC_InvokeCommand(o->session, TA_STORAGE_CMD_ALLOC_OBJ, &op, &org);
 	*obj = op.params[1].value.a;
 	return res;
 }
 
-TEEC_Result fs_free_obj(fs *f, uint32_t obj)
+TEEC_Result fs_free_obj(oc *o, uint32_t obj)
 {
 	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
 	uint32_t org = 0;
@@ -334,10 +325,10 @@ TEEC_Result fs_free_obj(fs *f, uint32_t obj)
 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_NONE,
 					 TEEC_NONE, TEEC_NONE);
 
-	return TEEC_InvokeCommand(f->session, TA_STORAGE_CMD_FREE_OBJ, &op, &org);
+	return TEEC_InvokeCommand(o->session, TA_STORAGE_CMD_FREE_OBJ, &op, &org);
 }
 
-TEEC_Result fs_reset_obj(fs *f, uint32_t obj)
+TEEC_Result fs_reset_obj(oc *o, uint32_t obj)
 {
 	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
 	uint32_t org = 0;
@@ -347,10 +338,10 @@ TEEC_Result fs_reset_obj(fs *f, uint32_t obj)
 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_NONE,
 					 TEEC_NONE, TEEC_NONE);
 
-	return TEEC_InvokeCommand(f->session, TA_STORAGE_CMD_RESET_OBJ, &op, &org);
+	return TEEC_InvokeCommand(o->session, TA_STORAGE_CMD_RESET_OBJ, &op, &org);
 }
 
-TEEC_Result fs_get_obj_info(fs *f, uint32_t obj,
+TEEC_Result fs_get_obj_info(oc *o, uint32_t obj,
 				void *obj_info, size_t info_size)
 {
 	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
@@ -364,17 +355,5 @@ TEEC_Result fs_get_obj_info(fs *f, uint32_t obj,
 	op.params[1].tmpref.buffer = obj_info;
 	op.params[1].tmpref.size = info_size;
 
-	return TEEC_InvokeCommand(f->session, TA_STORAGE_CMD_GET_OBJ_INFO, &op, &org);
-}
-
-void closeSession(fs *f)
-{
-	TEEC_CloseSession(f->session);
-	free(f->session);
-}
-
-void finalizeContext(fs *f)
-{
-	TEEC_FinalizeContext(f->ctx);
-	free(f->ctx);
+	return TEEC_InvokeCommand(o->session, TA_STORAGE_CMD_GET_OBJ_INFO, &op, &org);
 }
--- host/libfs/fs.h
+++ host/libfs/fs.h
@@ -2,53 +2,40 @@
 #define __FS_H__
 
 #include <tee_client_api.h>
+#include <session.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-	typedef struct _optee_fs_context {
-		TEEC_Context *ctx;
-		TEEC_Session *session;
-		uint32_t error;
-	} fs;
-
-
-	TEEC_Result initializeContext(const char *name,fs *f);
-	TEEC_Result openSession(fs *f,uint32_t connectionMethod,
-						const void *connectionData,TEEC_Operation *operation);
-
-
-	TEEC_Result fs_open(fs *f, void *id, uint32_t id_size,
+	TEEC_UUID * getUUID();
+	TEEC_Result fs_open(oc *o, void *id, uint32_t id_size,
 						uint32_t flags, uint32_t *obj, uint32_t storage_id);
-	TEEC_Result fs_create(fs *f, void *id, uint32_t id_size,
+	TEEC_Result fs_create(oc *o, void *id, uint32_t id_size,
 						uint32_t flags, uint32_t attr, void *data,
 						uint32_t data_size, uint32_t *obj,
 						uint32_t storage_id);
-	TEEC_Result fs_create_overwrite(fs *f, void *id,uint32_t id_size, uint32_t storage_id);
-	TEEC_Result fs_close(fs *f, uint32_t obj);
-	TEEC_Result fs_read(fs *f, uint32_t obj, void *data,uint32_t data_size, uint32_t *count);
-	TEEC_Result fs_write(fs *f, uint32_t obj, void *data,uint32_t data_size);
-	TEEC_Result fs_seek(fs *f, uint32_t obj, int32_t offset,int32_t whence);
-	TEEC_Result fs_unlink(fs *f, uint32_t obj);
-	TEEC_Result fs_trunc(fs *f, uint32_t obj, uint32_t len);
-	TEEC_Result fs_rename(fs *f, uint32_t obj, void *id,uint32_t id_size);
-	TEEC_Result fs_alloc_enum(fs *f, uint32_t *e);
-	TEEC_Result fs_reset_enum(fs *f, uint32_t e);
-	TEEC_Result fs_free_enum(fs *f, uint32_t e);
-	TEEC_Result fs_start_enum(fs *f, uint32_t e,uint32_t storage_id);
-	TEEC_Result fs_next_enum(fs *f, uint32_t e, void *obj_info,
+	TEEC_Result fs_create_overwrite(oc *o, void *id,uint32_t id_size, uint32_t storage_id);
+	TEEC_Result fs_close(oc *o, uint32_t obj);
+	TEEC_Result fs_read(oc *o, uint32_t obj, void *data,uint32_t data_size, uint32_t *count);
+	TEEC_Result fs_write(oc *o, uint32_t obj, void *data,uint32_t data_size);
+	TEEC_Result fs_seek(oc *o, uint32_t obj, int32_t offset,int32_t whence);
+	TEEC_Result fs_unlink(oc *o, uint32_t obj);
+	TEEC_Result fs_trunc(oc *o, uint32_t obj, uint32_t len);
+	TEEC_Result fs_rename(oc *o, uint32_t obj, void *id,uint32_t id_size);
+	TEEC_Result fs_alloc_enum(oc *o, uint32_t *e);
+	TEEC_Result fs_reset_enum(oc *o, uint32_t e);
+	TEEC_Result fs_free_enum(oc *o, uint32_t e);
+	TEEC_Result fs_start_enum(oc *o, uint32_t e,uint32_t storage_id);
+	TEEC_Result fs_next_enum(oc *o, uint32_t e, void *obj_info,
 						size_t info_size, void *id, uint32_t id_size);
-	TEEC_Result fs_restrict_usage(fs *f, uint32_t obj,uint32_t obj_usage);
-	TEEC_Result fs_alloc_obj(fs *f, uint32_t obj_type,
+	TEEC_Result fs_restrict_usage(oc *o, uint32_t obj,uint32_t obj_usage);
+	TEEC_Result fs_alloc_obj(oc *o, uint32_t obj_type,
 						uint32_t max_key_size, uint32_t *obj);
-	TEEC_Result fs_free_obj(fs *f, uint32_t obj);
-	TEEC_Result fs_reset_obj(fs *f, uint32_t obj);
-	TEEC_Result fs_get_obj_info(fs *f, uint32_t obj,
+	TEEC_Result fs_free_obj(oc *o, uint32_t obj);
+	TEEC_Result fs_reset_obj(oc *o, uint32_t obj);
+	TEEC_Result fs_get_obj_info(oc *o, uint32_t obj,
 						void *obj_info, size_t info_size);
-
-	void closeSession(fs *f);
-	void finalizeContext(fs *f);
 
 #ifdef __cplusplus
 }
