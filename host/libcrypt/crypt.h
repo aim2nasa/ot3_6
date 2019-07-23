e7ce0023ba4fe1be328e6889963864111dfb61fd
Rossi Kwak <rossi.kwak@gmail.com>
2019-07-23 19:43:14 +0900

    add crypto functions which are using TA_CRYPT_CMD_xxx from xtest_helpers.c ref.https://github.com/OP-TEE/optee_test/blob/3.6.0/host/xtest/xtest_helpers.c

--- host/libcrypt/crypt.h
+++ host/libcrypt/crypt.h
@@ -68,6 +68,20 @@ extern "C" {
 	TEEC_Result ta_crypt_cmd_generate_key(oc *o,TEE_ObjectHandle oh,
 					uint32_t key_size,const TEE_Attribute *params,uint32_t paramCount);
 
+	TEEC_Result ta_crypt_cmd_allocate_operation(oc *o,TEE_OperationHandle *oph,
+					uint32_t algo, uint32_t mode,uint32_t max_key_size);
+	TEEC_Result ta_crypt_cmd_allocate_transient_object(oc *o,TEE_ObjectType obj_type,
+					uint32_t max_obj_size,TEE_ObjectHandle *oh);
+	TEEC_Result ta_crypt_cmd_populate_transient_object(oc *o,TEE_ObjectHandle oh,
+					const TEE_Attribute *attrs,uint32_t attr_count);
+	TEE_Result ta_crypt_cmd_set_operation_key(oc *o,TEE_OperationHandle oph,TEE_ObjectHandle key);
+	TEEC_Result ta_crypt_cmd_free_transient_object(oc *o,TEE_ObjectHandle oh);
+	TEEC_Result ta_crypt_cmd_derive_key(oc *o,TEE_OperationHandle oph,
+					TEE_ObjectHandle oh,const TEE_Attribute *params,uint32_t paramCount);
+	TEEC_Result ta_crypt_cmd_get_object_buffer_attribute(oc *o,TEE_ObjectHandle oh,
+					uint32_t attr_id,void *buf, size_t *blen);
+	TEEC_Result ta_crypt_cmd_free_operation(oc *o,TEE_OperationHandle oph);
+
 #ifdef __cplusplus
 }
 #endif
--- host/libcrypt/helpers.c
+++ host/libcrypt/helpers.c
@@ -1,6 +1,9 @@
 #include "helpers.h"
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
+#include <session.h>
+#include <ta_crypt.h>
 
 /* Round up the even multiple of size, size has to be a multiple of 2 */
 #define ROUNDUP(v, size) (((v) + (size - 1)) & ~(size - 1))
@@ -68,4 +71,192 @@ TEE_Result pack_attrs(const TEE_Attribute *attrs, uint32_t attr_count,
 	}
 
 	return TEE_SUCCESS;
+}
+
+TEEC_Result ta_crypt_cmd_allocate_operation(oc *o, TEE_OperationHandle *oph,
+					    uint32_t algo, uint32_t mode, uint32_t max_key_size)
+{
+	TEEC_Result res = TEEC_ERROR_GENERIC;
+	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
+	uint32_t ret_orig = 0;
+
+	op.params[0].value.a = 0;
+	op.params[0].value.b = algo;
+	op.params[1].value.a = mode;
+	op.params[1].value.b = max_key_size;
+
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INOUT, TEEC_VALUE_INPUT,
+					 TEEC_NONE, TEEC_NONE);
+
+	res = TEEC_InvokeCommand(o->session, TA_CRYPT_CMD_ALLOCATE_OPERATION, &op,
+				 &ret_orig);
+
+	if (res == TEEC_SUCCESS)
+		*oph = (TEE_OperationHandle)(uintptr_t)op.params[0].value.a;
+
+	return res;
+}
+
+TEEC_Result ta_crypt_cmd_allocate_transient_object(oc *o, TEE_ObjectType obj_type,
+						   uint32_t max_obj_size, TEE_ObjectHandle *oh)
+{
+	TEEC_Result res = TEEC_ERROR_GENERIC;
+	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
+	uint32_t ret_orig = 0;
+
+	op.params[0].value.a = obj_type;
+	op.params[0].value.b = max_obj_size;
+
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_VALUE_OUTPUT,
+					 TEEC_NONE, TEEC_NONE);
+
+	res = TEEC_InvokeCommand(o->session, TA_CRYPT_CMD_ALLOCATE_TRANSIENT_OBJECT, &op,
+				 &ret_orig);
+
+	if (res == TEEC_SUCCESS)
+		*oh = (TEE_ObjectHandle)(uintptr_t)op.params[1].value.a;
+
+	return res;
+}
+
+TEEC_Result ta_crypt_cmd_populate_transient_object(oc *o, TEE_ObjectHandle oh,
+						   const TEE_Attribute *attrs, uint32_t attr_count)
+{
+	TEEC_Result res = TEEC_ERROR_GENERIC;
+	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
+	uint32_t ret_orig = 0;
+	uint8_t *buf = NULL;
+	size_t blen = 0;
+
+	res = pack_attrs(attrs, attr_count, &buf, &blen);
+	if (res!=TEEC_SUCCESS)
+		return res;
+
+	assert((uintptr_t)oh <= UINT32_MAX);
+	op.params[0].value.a = (uint32_t)(uintptr_t)oh;
+
+	op.params[1].tmpref.buffer = buf;
+	op.params[1].tmpref.size = blen;
+
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT,
+					 TEEC_MEMREF_TEMP_INPUT, TEEC_NONE,
+					 TEEC_NONE);
+
+	res = TEEC_InvokeCommand(o->session, TA_CRYPT_CMD_POPULATE_TRANSIENT_OBJECT, &op,
+				 &ret_orig);
+
+	free(buf);
+	return res;
+}
+
+TEE_Result ta_crypt_cmd_set_operation_key(oc *o, TEE_OperationHandle oph, TEE_ObjectHandle key)
+{
+	TEEC_Result res = TEEC_ERROR_GENERIC;
+	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
+	uint32_t ret_orig = 0;
+
+	assert((uintptr_t)oph <= UINT32_MAX);
+	op.params[0].value.a = (uint32_t)(uintptr_t)oph;
+
+	assert((uintptr_t)key <= UINT32_MAX);
+	op.params[0].value.b = (uint32_t)(uintptr_t)key;
+
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_NONE, TEEC_NONE,
+					 TEEC_NONE);
+
+	res = TEEC_InvokeCommand(o->session, TA_CRYPT_CMD_SET_OPERATION_KEY, &op,
+				 &ret_orig);
+	return res;
+}
+
+TEEC_Result ta_crypt_cmd_free_transient_object(oc *o, TEE_ObjectHandle oh)
+{
+	TEEC_Result res = TEEC_ERROR_GENERIC;
+	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
+	uint32_t ret_orig = 0;
+
+	assert((uintptr_t)oh <= UINT32_MAX);
+	op.params[0].value.a = (uint32_t)(uintptr_t)oh;
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_NONE, TEEC_NONE,
+					 TEEC_NONE);
+
+	res = TEEC_InvokeCommand(o->session, TA_CRYPT_CMD_FREE_TRANSIENT_OBJECT, &op,
+				 &ret_orig);
+	return res;
+}
+
+TEEC_Result ta_crypt_cmd_derive_key(oc *o,TEE_OperationHandle oph, TEE_ObjectHandle oh,
+					const TEE_Attribute *params, uint32_t paramCount)
+{
+	TEEC_Result res = TEEC_ERROR_GENERIC;
+	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
+	uint32_t ret_orig = 0;
+	uint8_t *buf = NULL;
+	size_t blen = 0;
+
+	res = pack_attrs(params, paramCount, &buf, &blen);
+
+	if (res!=TEEC_SUCCESS)
+		return res;
+
+	assert((uintptr_t)oph <= UINT32_MAX);
+	op.params[0].value.a = (uint32_t)(uintptr_t)oph;
+
+	assert((uintptr_t)oh <= UINT32_MAX);
+	op.params[0].value.b = (uint32_t)(uintptr_t)oh;
+
+	op.params[1].tmpref.buffer = buf;
+	op.params[1].tmpref.size = blen;
+
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT,
+					 TEEC_MEMREF_TEMP_INPUT, TEEC_NONE,
+					 TEEC_NONE);
+
+	res = TEEC_InvokeCommand(o->session, TA_CRYPT_CMD_DERIVE_KEY, &op, &ret_orig);
+
+	free(buf);
+	return res;
+}
+
+TEEC_Result ta_crypt_cmd_get_object_buffer_attribute(oc * o, TEE_ObjectHandle oh,
+					uint32_t attr_id, void *buf, size_t *blen)
+{
+	TEEC_Result res = TEEC_ERROR_GENERIC;
+	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
+	uint32_t ret_orig = 0;
+
+	assert((uintptr_t)oh <= UINT32_MAX);
+	op.params[0].value.a = (uint32_t)(uintptr_t)oh;
+	op.params[0].value.b = attr_id;
+
+	op.params[1].tmpref.buffer = buf;
+	op.params[1].tmpref.size = *blen;
+
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT,
+					 TEEC_MEMREF_TEMP_OUTPUT, TEEC_NONE,
+					 TEEC_NONE);
+
+	res = TEEC_InvokeCommand(o->session, TA_CRYPT_CMD_GET_OBJECT_BUFFER_ATTRIBUTE,
+				 &op, &ret_orig);
+
+	if (res == TEEC_SUCCESS)
+		*blen = op.params[1].tmpref.size;
+
+	return res;
+}
+
+TEEC_Result ta_crypt_cmd_free_operation(oc *o, TEE_OperationHandle oph)
+{
+	TEEC_Result res = TEEC_ERROR_GENERIC;
+	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
+	uint32_t ret_orig = 0;
+
+	op.params[0].value.a = (uint32_t)(uintptr_t)oph;
+
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_NONE, TEEC_NONE,
+					 TEEC_NONE);
+
+	res = TEEC_InvokeCommand(o->session, TA_CRYPT_CMD_FREE_OPERATION, &op,
+				 &ret_orig);
+	return res;
 }
