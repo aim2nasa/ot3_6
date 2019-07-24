f95e171e1f62169d6770718068d224dfcec8c92f
Rossi Kwak <rossi.kwak@gmail.com>
2019-07-24 17:54:14 +0900

    implement ta_key_cmd_generate by copying from 'ta_key_gen_cmd of ot2.6/keygen/ta/keygen.c' ref. http://220.230.122.228/redmine/projects/wc300/repository/ot/revisions/master/entry/keygen/ta/keygen.c#L6

--- ta/key/ta_entry.c
+++ ta/key/ta_entry.c
@@ -1,5 +1,10 @@
 #include <tee_ta_api.h>
 #include <ta_key.h>
+#include <common.h>
+#include <trace.h>
+#include <tee_api.h>
+#include <stdlib.h>
+#include <string.h>
 
 /*
  * Trusted Application Entry Points
@@ -35,8 +40,69 @@ void TA_CloseSessionEntryPoint(void *pSessionContext)
 
 static TEE_Result ta_key_cmd_generate(uint32_t param_types, TEE_Param params[4])
 {
-	TEE_Result res = TEE_ERROR_GENERIC;
-	return res;
+	TEE_Result result = TEE_SUCCESS;
+	TEE_ObjectHandle transient_key = (TEE_ObjectHandle)NULL;
+	TEE_ObjectHandle persistent_key = (TEE_ObjectHandle)NULL;
+	size_t key_size;
+	TEE_ObjectInfo keyInfo;
+	char *keyFileName = 0;
+	uint32_t flags;
+
+	(void)params;
+	ASSERT_PARAM_TYPE(TEE_PARAM_TYPES
+			  (TEE_PARAM_TYPE_VALUE_INPUT,TEE_PARAM_TYPE_MEMREF_INPUT,
+			  TEE_PARAM_TYPE_VALUE_INPUT,TEE_PARAM_TYPE_VALUE_INPUT));
+
+	key_size = params[3].value.a;
+	DMSG("received key size: %zd bits",key_size);
+	if(key_size!=128 && key_size!=192 && key_size!=256) {
+		DMSG("key size: %zd bits, it must be one of 128,192,256.Otherwise not supported",key_size);
+		return TEE_ERROR_NOT_SUPPORTED;
+	}
+
+	if((result=TEE_AllocateTransientObject(TEE_TYPE_AES,key_size,&transient_key))!=TEE_SUCCESS){
+		EMSG("Failed to Allocate transient object handle : 0x%x",result);
+		goto cleanup1;
+	}
+	DMSG("Allocated TransientObject: %p",(void*)transient_key);
+
+
+	if((result=TEE_GenerateKey(transient_key,key_size,NULL,0))!=TEE_SUCCESS){
+		EMSG("Failed to generate a transient key: 0x%x", result);
+		goto cleanup2;
+	}
+	DMSG("Key(%zd bit) generated with TransientObject(%p)",key_size,(void*)transient_key);
+
+	TEE_GetObjectInfo1(transient_key, &keyInfo);
+	DMSG("keyInfo: %zd bytes",sizeof(TEE_ObjectInfo));
+
+	DMSG("Input params[0], storage id: %d",params[0].value.a);
+
+	if(params[1].memref.size>0) {
+		keyFileName = malloc(params[1].memref.size+1);
+		memcpy(keyFileName,params[1].memref.buffer,params[1].memref.size);
+		keyFileName[params[1].memref.size]=0;
+		DMSG("Input params[1], key filename: %s",keyFileName);
+
+		flags = params[2].value.a;
+		DMSG("Input params[2], flags:0x%x",flags);
+		if((result=TEE_CreatePersistentObject(params[0].value.a,
+						      params[1].memref.buffer,params[1].memref.size,
+						      flags,transient_key,NULL,0,&persistent_key))!=TEE_SUCCESS){
+			EMSG("Failed to create a persistent key: 0x%x", result);
+			goto cleanup2;
+		}
+		DMSG("%s persistent object(%p) flags:0x%x created",keyFileName,(void*)persistent_key,flags);
+
+		TEE_CloseObject(persistent_key);
+	}else{
+		DMSG("persistent object can't be created, no object name specified");
+	}
+cleanup2:
+	TEE_FreeTransientObject(transient_key);
+cleanup1:
+	free(keyFileName);
+	return result;
 }
 
 /* Called when a command is invoked */
