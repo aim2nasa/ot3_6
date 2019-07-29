241ed66b2cf34d531f9a35328c8f1f8ce8d0d289
Rossi Kwak <rossi.kwak@gmail.com>
2019-07-29 18:58:00 +0900

    add new function 'keyEnumObjectList' by using the libfs

--- host/libkey/key.c
+++ host/libkey/key.c
@@ -1,5 +1,7 @@
 #include "key.h"
 #include <string.h>
+#include <fs.h>
+#include <stdlib.h>
 
 TEEC_Result keyGen(oc *o,storageId sid,const char *keyFileName,uint32_t flags,uint32_t keySize)
 {
@@ -13,4 +15,51 @@ TEEC_Result keyGen(oc *o,storageId sid,const char *keyFileName,uint32_t flags,ui
 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT,TEEC_MEMREF_TEMP_INPUT,TEEC_VALUE_INPUT,TEEC_VALUE_INPUT);
 
 	return TEEC_InvokeCommand(o->session,TA_KEY_CMD_GENERATE,&op,&o->error);
+}
+
+TEEC_Result keyEnumObjectList(oc *o,storageId sid,eObjList **list)
+{
+	TEEC_Result res;
+	uint32_t enumHandle;
+	size_t infoSize;
+	uint32_t idSize;
+	uint8_t info[256];
+	uint8_t id[256+1];        //+1 to include null at the end of name
+
+	eObjList *prev = NULL;
+	*list = NULL;
+
+	res = fs_alloc_enum(o,&enumHandle);
+	if(res!=TEEC_SUCCESS) return res;
+
+	res = fs_start_enum(o,enumHandle,sid);
+	if(res!=TEEC_SUCCESS) return res;
+
+	memset((void*)info,0,sizeof(info));
+	memset((void*)id,0,sizeof(id));
+	infoSize = sizeof(info);
+	idSize = sizeof(id);
+
+	while(TEEC_SUCCESS==fs_next_enum(o,enumHandle,info,infoSize,id,idSize)){
+		eObj *obj = NULL;
+		eObjList *objectList = (eObjList*)malloc(sizeof(eObjList));
+
+		if(*list==NULL) *list = objectList; 
+
+		obj = (eObj*)malloc(sizeof(eObj));
+		obj->idSize = idSize;
+		obj->id = (uint8_t*)malloc(idSize);
+
+		memcpy(obj->id,id,idSize);
+		objectList->object = obj;
+		objectList->next = NULL;
+		if(prev) prev->next = objectList;
+
+		prev = objectList;
+		infoSize = sizeof(info);
+		idSize = sizeof(id);
+	}
+	res = fs_free_enum(o,enumHandle);
+	if(res!=TEEC_SUCCESS) return res;
+	return res;
 }
