f3c7b7928fa077a6612500f6df0ed6597ae3fffc
Rossi Kwak <rossi.kwak@gmail.com>
2019-08-01 19:33:03 +0900

    copy fs_xxxx_enum functions from libfs

--- host/libkey/key.c
+++ host/libkey/key.c
@@ -24,3 +24,82 @@ TEEC_Result keyList(oc *o,storageId sid,eObjList **list)
 
 	return TEEC_InvokeCommand(o->session,TA_KEY_CMD_LIST,&op,&o->error);
 }
+
+TEEC_Result fs_alloc_enum(oc *o, uint32_t *e)
+{
+	TEEC_Result res = TEEC_ERROR_GENERIC;
+	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
+	uint32_t org = 0;
+
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_OUTPUT, TEEC_NONE,
+					 TEEC_NONE, TEEC_NONE);
+
+	res = TEEC_InvokeCommand(o->session, TA_KEY_CMD_ALLOC_ENUM, &op, &org);
+
+	if (res == TEEC_SUCCESS)
+		*e = op.params[0].value.a;
+
+	return res;
+}
+
+TEEC_Result fs_reset_enum(oc *o, uint32_t e)
+{
+	TEEC_Result res = TEEC_ERROR_GENERIC;
+	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
+	uint32_t org = 0;
+
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_NONE,
+					 TEEC_NONE, TEEC_NONE);
+
+	op.params[0].value.a = e;
+	res = TEEC_InvokeCommand(o->session, TA_KEY_CMD_RESET_ENUM, &op, &org);
+
+	return res;
+}
+
+TEEC_Result fs_free_enum(oc *o, uint32_t e)
+{
+	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
+	uint32_t org = 0;
+
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_NONE, TEEC_NONE,
+					 TEEC_NONE);
+
+	op.params[0].value.a = e;
+
+	return TEEC_InvokeCommand(o->session, TA_KEY_CMD_FREE_ENUM, &op, &org);
+}
+
+TEEC_Result fs_start_enum(oc *o, uint32_t e,uint32_t storage_id)
+{
+	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
+	uint32_t org = 0;
+
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_NONE,
+					 TEEC_NONE, TEEC_NONE);
+
+	op.params[0].value.a = e;
+	op.params[0].value.b = storage_id;
+
+	return TEEC_InvokeCommand(o->session, TA_KEY_CMD_START_ENUM, &op, &org);
+}
+
+TEEC_Result fs_next_enum(oc *o, uint32_t e, void *obj_info,
+						 size_t info_size, void *id, uint32_t id_size)
+{
+	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
+	uint32_t org = 0;
+
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_NONE,
+					 TEEC_MEMREF_TEMP_OUTPUT, TEEC_NONE);
+	if (obj_info && info_size)
+		op.paramTypes |= (TEEC_MEMREF_TEMP_OUTPUT << 4);
+
+	op.params[0].value.a = e;
+	op.params[1].tmpref.buffer = obj_info;
+	op.params[1].tmpref.size = info_size;
+	op.params[2].tmpref.buffer = id;
+	op.params[2].tmpref.size = id_size;
+
+	return TEEC_InvokeCommand(o->session, TA_KEY_CMD_NEXT_ENUM, &op, &org);
+}
