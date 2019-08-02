10734fc6c6f152c358a875a20fa98a6ab5d64265
Rossi Kwak <rossi.kwak@gmail.com>
2019-08-02 16:26:12 +0900

    When creating keyEnumObjectList, retrieve the size of the list through the argument and make tests to validate the change

--- host/libkey/key.h
+++ host/libkey/key.h
@@ -38,7 +38,7 @@ extern "C" {
 	TEEC_Result keyStartEnum(oc *o, uint32_t e,uint32_t storage_id);
 	TEEC_Result keyNextEnum(oc *o, uint32_t e, void *obj_info,
 							 size_t *info_size, void *id, uint32_t *id_size);
-	TEEC_Result keyEnumObjectList(oc *o,storageId sid,eObjList **list);
+	TEEC_Result keyEnumObjectList(oc *o,storageId sid,eObjList **list,size_t *listSize);
 	size_t keyFreeEnumObjectList(eObjList **list);
 
 #ifdef __cplusplus
